005983bd203cd0674d02faea4bce652536579855
Gbustarr <gbustamante21@alumnos.utalca.cl>
2024-06-15 20:03:40 -0400

    Agregando archivo .sh para cambiar la ip almacenada en mysql

---
+++ data/prestashop_DB/ip_setup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Verificar si la variable de entorno PRESTASHOP_IP está establecida
+if [ -z "$PRESTASHOP_IP" ]; then
+  echo "La variable de entorno PRESTASHOP_IP no está establecida."
+  exit 1
+fi
+
+echo "Estableciendo variables de entorno"
+
+# Configuración de conexión a la base de datos
+DB_USER="root"
+DB_PASSWORD=$MYSQL_ROOT_PASSWORD
+DB_NAME="prestashop"
+
+echo user $DB_USER, pass $DB_PASSWORD, DB $DB_NAME , IP $PRESTASHOP_IP
+
+# Consulta SQL que se desea ejecutar
+SQL_QUERY="UPDATE ps_configuration SET value = '${PRESTASHOP_IP}' WHERE name IN ('PS_SHOP_DOMAIN', 'PS_SHOP_DOMAIN_SSL');"
+
+# Ejecutar la consulta SQL utilizando la variable de entorno
+mysql -u $DB_USER -p$DB_PASSWORD
+
+# Verificar si el comando mysql se ejecutó correctamente
+if [ $? -eq 0 ]; then
+  echo "Consulta ejecutada con éxito."
+else
+  echo "Error al ejecutar la consulta."
+  exit 1
+fi
+
+
