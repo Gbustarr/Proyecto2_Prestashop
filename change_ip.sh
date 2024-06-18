308d863f90861fad71a5918189fff8ba83a351af
Gbustarr <gbustamante21@alumnos.utalca.cl>
2024-06-18 09:22:02 -0400

    Agregado cambio de permisos en el script de cambio de ip

--- change_ip.sh
+++ change_ip.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 echo "Entrando a contenedor y cambiando ip de prestashop"
 docker exec -it contenedor_db /bin/bash /ip_setup.sh
+docker exec -it contenedor_prestashop /bin/bash -c "chown -R www-data:www-data /var/www/html"
+docker exec -it contenedor_prestashop /bin/bash -c "chmod -R 755 /var/www/html"
 echo "Script ejecutado"
